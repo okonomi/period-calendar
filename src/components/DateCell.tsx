9a02e8f4b158c8620121393bfefb3274d0badb10
okonomi <okonomi@oknm.jp>
2025-03-15 18:32:08 +0900

    style: remove unused serialNumber prop from DateCell component

--- src/components/DateCell.tsx
+++ src/components/DateCell.tsx
@@ -1,14 +1,12 @@
 // 型定義
 export type Props = {
   date: Date;
-  serialNumber: number;
 };
 
 // 日付表示用のコンポーネント
-export const DateCell: React.FC<Props> = ({ date, serialNumber }) => {
+export const DateCell: React.FC<Props> = ({ date }) => {
   return (
     <div className="p-2 rounded bg-white">
-      <div className="text-xs text-gray-600">{serialNumber}</div>
       <div className="text-center text-black">
         {date.getMonth() + 1}/{date.getDate()}
       </div>
