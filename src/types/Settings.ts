9e72534f9f6b6a46cf78ad26c68a301456c35e19
okonomi <okonomi@oknm.jp>
2025-03-21 22:13:33 +0900

    refactor: remove displayMode from Settings interface and default settings

--- src/types/Settings.ts
+++ src/types/Settings.ts
@@ -1,6 +1,4 @@
 export interface Settings {
-  // カレンダーの表示設定
-  displayMode: "normal" | "compact"
   // 休日の表示設定
   showHolidays: boolean
   // 週末の表示設定
@@ -17,7 +15,6 @@ export type SettingsContextType = {
 
 // デフォルト設定
 export const defaultSettings: Settings = {
-  displayMode: "normal",
   showHolidays: true,
   highlightWeekends: true,
   firstPeriodStartYear: 1999,
