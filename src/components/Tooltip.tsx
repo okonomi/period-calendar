e6537adb27bd0b16ab1d08b252372426bde4c270
okonomi <okonomi@oknm.jp>
2025-03-22 00:46:34 +0900

    refactor: consolidate tooltip logic into TooltipContainer and remove useTooltipPosition hook

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,22 +1,20 @@
 import type { CSSProperties } from "react"
 
-export type TooltipPosition = { top: number; left: number }
-
 type Props = {
   text: string
-  position: TooltipPosition
+  position: { top: number; left: number }
 }
 
 export const Tooltip: React.FC<Props> = ({ text, position }) => {
-  // top, leftは動的な値なのでstyleとして残す
   const style: CSSProperties = {
+    position: "absolute",
     top: position.top,
     left: position.left,
   }
 
   return (
     <div
-      className="absolute -translate-x-1/2 whitespace-nowrap bg-black text-white text-xs rounded py-1 px-2"
+      className="absolute -translate-x-1/2 whitespace-nowrap bg-black text-white text-xs rounded py-1 px-2 before:content-[''] before:absolute before:left-1/2 before:-translate-x-1/2 before:bottom-[-4px] before:border-4 before:border-transparent before:border-t-black"
       style={style}
     >
       {text}
--- src/components/TooltipContainer.tsx
+++ src/components/TooltipContainer.tsx
@@ -1,15 +1,27 @@
-import { useTooltipPosition } from "../hooks/use-tooltip-position"
+import { useState } from "react"
 import { Tooltip } from "./Tooltip"
+
+type TooltipPosition = { top: number; left: number }
 
 type Props = {
   tooltip?: string
 }
 
 export const TooltipContainer: React.FC<React.PropsWithChildren<Props>> = ({ children, tooltip }) => {
-  const { tooltipPosition, handleMouseEnter, handleMouseLeave } = useTooltipPosition()
+  const [tooltipPosition, setTooltipPosition] = useState<TooltipPosition | null>(null)
+
+  const handleMouseEnter = (event: React.MouseEvent) => {
+    if (!tooltip) return
+    const rect = event.currentTarget.getBoundingClientRect()
+    setTooltipPosition({ top: -30, left: rect.width / 2 })
+  }
+
+  const handleMouseLeave = () => {
+    setTooltipPosition(null)
+  }
 
   return (
-    <div className="relative" onMouseEnter={(e) => !!tooltip && handleMouseEnter(e)} onMouseLeave={handleMouseLeave}>
+    <div className="relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       {children}
       {tooltipPosition && tooltip && <Tooltip text={tooltip} position={tooltipPosition} />}
     </div>
