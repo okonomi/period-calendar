ec0f245bb30a50464ae6e3620d962f4af90568b3
okonomi <okonomi@oknm.jp>
2025-03-27 23:41:15 +0900

    chore: add Prettier configuration and update extension recommendations

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: false,
+  printWidth: 120,
+  trailingComma: "es5",
+  plugins: ["prettier-plugin-tailwindcss"],
+}
+
+export default config
