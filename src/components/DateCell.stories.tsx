ba52335c2707916e650c2dd77b1a83c8d2f90972
okonomi <okonomi@oknm.jp>
2025-03-30 18:32:03 +0900

    feat: add decorators for HolidaysContext in DateCell stories

--- src/components/DateCell.stories.tsx
+++ src/components/DateCell.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react"
 
+import { HolidaysContext } from "../contexts/HolidaysContext"
 import { createCalendarDateFromDate } from "../domain/CalendarDate"
 import { DateCell } from "./DateCell"
 
@@ -10,6 +11,13 @@ const meta = {
   parameters: {
     layout: "fullscreen",
   },
+  decorators: [
+    (Story) => (
+      <HolidaysContext.Provider value={{}}>
+        <Story />
+      </HolidaysContext.Provider>
+    ),
+  ],
 } satisfies Meta<typeof DateCell>
 
 export default meta
@@ -19,5 +27,32 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {
     date: createCalendarDateFromDate(new Date(2023, 1, 1)),
+  },
+}
+
+export const Holiday: Story = {
+  args: {
+    date: createCalendarDateFromDate(new Date(2023, 1, 1)),
+  },
+  decorators: [
+    (Story) => {
+      const mockHoliday = {
+        "2023-02-01": {
+          date: createCalendarDateFromDate(new Date(2023, 1, 1)),
+          name: "Holiday Example",
+        },
+      }
+      return (
+        <HolidaysContext.Provider value={mockHoliday}>
+          <Story />
+        </HolidaysContext.Provider>
+      )
+    },
+  ],
+}
+
+export const Today: Story = {
+  args: {
+    date: createCalendarDateFromDate(new Date()),
   },
 }
